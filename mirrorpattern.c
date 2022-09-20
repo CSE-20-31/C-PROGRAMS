00c0d8258bbf16a1852c7497830d947da8acdfdd
CSE-20-31 <95126640+CSE-20-31@users.noreply.github.com>
2022-09-20 18:59:04 +0530

    Add files via upload

---
+++ mirrorpattern.c
@@ -0,0 +1,30 @@
+int main()
+{
+    int i,j,rows;
+    printf("Enter the number of rows you want to print: ");
+    scanf("%d", &rows);
+
+    printf("Printing the pattern: ");
+    printf("\n");
+
+    for(i=1; i<=rows; i++)
+    {
+         for(j=1; j<=rows; j++)
+         {
+             if(i+j<=rows)
+             {
+                 printf(" ");
+             }
+             else
+             {
+                 printf("* ");
+             }
+
+         }
+         printf("\n");
+
+    }
+
+
+    return 0;
+}
